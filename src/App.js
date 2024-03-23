f73d9edb273f63d8e6829a047aede531fa543b1c
Isaac <isaacong1@hotmail.com>
2024-03-23 23:42:04 +0800

    Add Author Name Form functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const [ authors , setAuthors ] = useState([]);
+  const [ data, setData ] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('https://jsonplaceholder.typicode.com/users');
+      const body = await result.json();
+      setData(body)
+    }
+
+    fetchData();
+    console.log(data);
+  }, [])
+
+  const userList = data.map((user, index) => {
+    return (
+      <li key={index}>
+        {user.name}
+      </li>
+    )
+  });
+
+  const nameList = authors.map((author, index) => {
+    return (
+      <li key={index}>
+        {author}
+      </li>
+    )
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NameInput names={authors} setNames={setAuthors} />
+        <ol>
+          {nameList}
+        </ol>
+        <ol>
+          {userList}
+        </ol>
       </header>
     </div>
   );
 }
 
-export default App;
+function NameInput({ names , setNames }) {
+  const [ input, setInput ] = useState("");
+  const [ errorText, setErrorText ] = useState("");
+
+  function handleChange(input) {
+    setInput(input);
+  }
+
+  function handleSubmit() {
+    if (input.length === 0) {
+      setErrorText("Please enter a name");
+      return;
+    }
+    console.log(input);
+    let nextNames = names.slice();
+    nextNames = [...nextNames, input];
+    setNames(nextNames);
+    setErrorText("");
+    setInput("");
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      handleSubmit();
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <div> Author Name </div>
+        <input 
+          value={input}
+          placeholder='Please enter name' 
+          onChange={(event) => handleChange(event.target.value)}
+          onKeyDown={(event) => handleKeyDown(event)}
+          />
+      </div>
+      <button onClick={handleSubmit}>
+        Submit 
+      </button>
+      <div> {errorText} </div>
+    </div>
+  )
+}
